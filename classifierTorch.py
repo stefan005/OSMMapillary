02b7855e65748b2c6eb03fa399f24b662627ab8e
stefan <dev@stkraus.de>
2020-12-26 22:48:37 +0100

    add split train/test directory and dataset

--- classifierTorch.py
+++ classifierTorch.py
@@ -1,61 +1,76 @@
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn, optim
 from torchvision import datasets, models, transforms
 
 data_dir = 'ressources/mapillary_raw/validated'
+test_dir = 'ressources/mapillary_raw/test'
 
-def make_weights_for_balanced_classes(images, nclasses):                        
-    count = [0] * nclasses                                                      
-    for item in images:                                                         
-        count[item[1]] += 1                                                     
-    weight_per_class = [0.] * nclasses                                      
-    N = float(sum(count))                                                   
-    for i in range(nclasses):                                                   
-        weight_per_class[i] = N/float(count[i])                                 
-    weight = [0] * len(images)                                              
-    for idx, val in enumerate(images):                                          
-        weight[idx] = weight_per_class[val[1]]                                  
-    return weight  
+def make_weights_for_balanced_classes(images, nclasses):
+    count = [0] * nclasses
+    for item in images:
+        count[item[1]] += 1
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))
+    for i in range(nclasses):
+        weight_per_class[i] = N/float(count[i])
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+    return weight
 
-def load_split_train_test(datadir, valid_size=.2):
-    train_transforms = transforms.Compose([transforms.Resize((224, 224)),
-                                           transforms.ToTensor(), ])
-    test_transforms = transforms.Compose([transforms.Resize((224, 224)),
-                                          transforms.ToTensor(), ])
+
+def load_split_train_test(datadir, testdir="", valid_size=.2):
+
+    train_transforms = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                            std=[0.229, 0.224, 0.225]),
+                                            ])
+    test_transforms = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                            std=[0.229, 0.224, 0.225]),
+                                            ])
     train_data = datasets.ImageFolder(datadir,
                                       transform=train_transforms)
-    test_data = datasets.ImageFolder(datadir,
-                                     transform=test_transforms)
-    num_train = len(train_data)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-    np.random.shuffle(indices)
-    from torch.utils.data.sampler import SubsetRandomSampler
-    train_idx, test_idx = indices[split:], indices[:split]
-    #for using weight a solution for subset must be found, otherwise, test data is included.
-    #weights = make_weights_for_balanced_classes(train_data.imgs, len(train_data.classes))                                                                
-    #weights = torch.DoubleTensor(weights)                                       
-    #train_sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(train_data))                     
-    train_sampler = SubsetRandomSampler(train_idx)
-    test_sampler = SubsetRandomSampler(test_idx)
+    if testdir != "":
+        test_data = datasets.ImageFolder(testdir,
+                                         transform=test_transforms)
+        weights = make_weights_for_balanced_classes(
+            train_data.imgs, len(train_data.classes))
+        weights = torch.DoubleTensor(weights)
+        train_sampler = torch.utils.data.sampler.WeightedRandomSampler(
+            weights, len(train_data))
+        test_sampler = torch.utils.data.sampler.RandomSampler(test_data)
+    else:
+        test_data = datasets.ImageFolder(datadir,
+                                         transform=test_transforms)
+        num_train = len(train_data)
+        indices = list(range(num_train))
+        split = int(np.floor(valid_size * num_train))
+        np.random.shuffle(indices)
+        from torch.utils.data.sampler import SubsetRandomSampler
+        train_idx, test_idx = indices[split:], indices[:split]
+        train_sampler = SubsetRandomSampler(train_idx)
+        test_sampler = SubsetRandomSampler(test_idx)
+
     trainloader = torch.utils.data.DataLoader(train_data,
-                                              sampler=train_sampler, batch_size=64,
-                                              persistent_workers=True, num_workers=2)
+                                              sampler=train_sampler, batch_size=64)
     testloader = torch.utils.data.DataLoader(test_data,
                                              sampler=test_sampler, batch_size=64)
     return trainloader, testloader
 
-trainloader, testloader = load_split_train_test(data_dir, .2)
+trainloader, testloader = load_split_train_test(data_dir, test_dir)
 print(trainloader.dataset.classes)
 print(testloader.dataset.classes)
 
 device = torch.device("cuda" if torch.cuda.is_available()
                       else "cpu")
 model = models.resnet50(pretrained=True)
-print(model)
+# print(model)
 
 for param in model.parameters():
     param.requires_grad = False
@@ -63,10 +78,11 @@ for param in model.parameters():
 model.fc = nn.Sequential(nn.Linear(2048, 512),
                          nn.ReLU(),
                          nn.Dropout(0.2),
-                         nn.Linear(512, 10),
+                         nn.Linear(512, len(trainloader.dataset.classes)),
                          nn.LogSoftmax(dim=1))
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+#torch.load(model, 'test-model.pth')
 model.to(device)
 
 epochs = 5
@@ -74,6 +90,7 @@ steps = 0
 running_loss = 0
 print_every = 10
 train_losses, test_losses = [], []
+starttime = datetime.datetime.now()
 for epoch in range(epochs):
     for inputs, labels in trainloader:
         steps += 1
@@ -88,7 +105,8 @@ for epoch in range(epochs):
         if steps % print_every == 0:
             test_loss = 0
             accuracy = 0
-            cmt = torch.zeros(len(trainloader.dataset.classes), len(trainloader.dataset.classes), dtype=torch.int64)
+            cmt = torch.zeros(len(trainloader.dataset.classes), len(
+                trainloader.dataset.classes), dtype=torch.int64)
             model.eval()
             with torch.no_grad():
                 for inputs, labels in testloader:
@@ -101,20 +119,23 @@ for epoch in range(epochs):
                     top_p, top_class = ps.topk(1, dim=1)
                     equals = top_class == labels.view(*top_class.shape)
                     accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-                    stacked = torch.stack((labels.reshape((-1,1)), top_class), dim=1)
-                    
+                    stacked = torch.stack(
+                        (labels.reshape((-1, 1)), top_class), dim=1)
+
                     for p in stacked:
                         tl, pl = p.tolist()
                         cmt[tl, pl] = cmt[tl, pl] + 1
-                    
+
             train_losses.append(running_loss/len(trainloader))
             test_losses.append(test_loss/len(testloader))
-            print(f"Epoch {epoch+1}/{epochs}.. "
+            print(f"now: {datetime.datetime.now().time()} "
+                  f"duration: {datetime.datetime.now()-starttime} "
+                  f"\nEpoch {epoch+1}/{epochs}.. "
                   f"Train loss: {running_loss/print_every:.3f}.. "
                   f"Test loss: {test_loss/len(testloader):.3f}.. "
                   f"Test accuracy: {accuracy/len(testloader):.3f}"
                   f"\nConfusion Matrix:\n {cmt}"
-                  f"\nclasses: {trainloader.dataset.classes}")
+                  f"\nclasses: {trainloader.dataset.classes}\n\n")
             running_loss = 0
             model.train()
 torch.save(model, 'test-model.pth')
